0bdb42107288aba4dca02f9e8e3fa03856eada57
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-11 00:45:08 +0200

    New update 0:44(11.01.22)

--- src/KK/Streams/JoeJames1/Two/Program.java
+++ src/KK/Streams/JoeJames1/Two/Program.java
@@ -1,16 +1,21 @@
 package KK.Streams.JoeJames1.Two;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Program {
-    public static void main(String[] args) {
-        List<Integer> number = Arrays.asList(2,3,4,5);
-        List square = number.stream()
-                .map(x -> x*x)
-                .collect(Collectors.toList());
-        System.out.println(square);
+    public static void main(String[] args) throws IOException {
+        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+        bands
+                .sorted()
+                .filter(x -> x.length() > 13)
+                .forEach(System.out::println);
+        bands.close();
 
     }
 }
