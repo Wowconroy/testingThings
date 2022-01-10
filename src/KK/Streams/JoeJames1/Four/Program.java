6c838b380a3c85727a02ad834d571e6fab2aa78c
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-11 01:16:44 +0200

    New update 1:46(11.01.22)

--- src/KK/Streams/JoeJames1/Four/Program.java
+++ src/KK/Streams/JoeJames1/Four/Program.java
@@ -1,8 +1,10 @@
 package KK.Streams.JoeJames1.Four;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
@@ -20,5 +22,9 @@ public class Program {
                 .reduce((s1,s2) -> s1 + "<-->" + s2);
 
         reduced.ifPresent(System.out::println);
+
+        List<Integer> members = Arrays.asList(1,2, 3, 4);
+        members.stream().reduce((x, y) -> x*y).ifPresent(System.out::println);
+
     }
 }
