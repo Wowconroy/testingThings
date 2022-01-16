147e473cc3c51703ee64329ce176edcb03619957
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-16 19:29:48 +0200

    New update 19:29 (16.01.22). Stream API

--- src/KK/Streams/JoeJames1/Eight/LessonsEight.java
+++ src/KK/Streams/JoeJames1/Eight/LessonsEight.java
@@ -1,12 +1,21 @@
 package KK.Streams.JoeJames1.Eight;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class LessonsEight {
+
     public static void main(String[] args) {
-        Stream.of("10", "11", "33")
-                .map(x -> Integer.parseInt(x, 16))
-                .forEach(System.out::println);
+        Stream<String> tokenStream = Arrays.asList("A", "B", "C", "D").stream();
+        String [] arrString = tokenStream.toArray(String[]::new);
+        System.out.println(Arrays.toString(arrString));
+
+        IntStream infiniteNumberStream = IntStream.iterate(1, i -> i+1);
+        int[] intArray = infiniteNumberStream.limit(10)
+                .toArray();
+        System.out.println(Arrays.toString(intArray));
     }
 }
