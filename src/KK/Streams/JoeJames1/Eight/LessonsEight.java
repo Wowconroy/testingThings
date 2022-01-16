445bff9a855c2035852a738b03110a4322e171eb
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-16 15:57:19 +0200

    New update 15:57 (16.01.22). Postgres

--- src/KK/Streams/JoeJames1/Eight/LessonsEight.java
+++ src/KK/Streams/JoeJames1/Eight/LessonsEight.java
@@ -1,7 +1,12 @@
 package KK.Streams.JoeJames1.Eight;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class LessonsEight {
     public static void main(String[] args) {
-
+        Stream.of("10", "11", "33")
+                .map(x -> Integer.parseInt(x, 16))
+                .forEach(System.out::println);
     }
 }
