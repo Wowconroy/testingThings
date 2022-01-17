1ef97a747f8ca137f71b8139223c07347edfeddb
Oleksii Shostak <shostak.ok@gmail.com>
2022-01-17 10:41:41 +0200

    New update 10:41 (17.01.22). Stream API

--- src/KK/Streams/JoeJames1/Eight/LessonsEight.java
+++ src/KK/Streams/JoeJames1/Eight/LessonsEight.java
@@ -49,5 +49,16 @@ public class LessonsEight {
                 })
                 .forEach(System.out::println);
 
+        Stream.of(0, 3, 0, 0, 5)
+                .peek(x -> System.out.format("before distinct: %d%n", x))
+                .distinct()
+                .peek(x -> System.out.format("after distinct: %d%n", x))
+                .map(x -> x * x)
+                .forEach(x -> System.out.format("after map: %d%n", x));
+
+        Stream.of(1, 2, 3, 4, 2, 5)
+                .takeWhile(x -> x < 3)
+                .forEach(System.out::println);
+
     }
 }
